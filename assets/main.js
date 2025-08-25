dac65190ba0d560b05b190f05257073cfdc875bc
Vivian Pupo <214867100+vivjo17@users.noreply.github.com>
2025-08-25 13:40:04 -0500

    added main.js

---
+++ assets/main.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const toggle = document.querySelector(".menu-toggle");
+  const nav = document.querySelector(".nav");
+  if (toggle && nav) {
+    toggle.addEventListener("click", () => {
+      nav.classList.toggle("active");
+    });
+  }
+});
